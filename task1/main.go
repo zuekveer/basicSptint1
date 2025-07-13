019db0f4f84733d96b9215eca2e38ef9b8ef0594
zuekveer <simetramonk@gmail.com>
2025-07-13 09:44:10 +0300

    canvas

---
+++ task1/main.go
@@ -0,0 +1,8 @@
+// Напиши минимальную Go-программу, которая выводит строку: Hello, World!
+package task1
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ task5/main.go
@@ -0,0 +1,16 @@
+/*
+	Что произойдёт при нескольких append() из одного исходного среза?
+
+Что выведет этот код и почему?
+
+	func main() {
+	var x []int
+	x = append(x, 0)
+	x = append(x, 1)
+	x = append(x, 2)
+	y := append(x, 3)
+	z := append(x, 4)
+	fmt.Println(y)
+	fmt.Println(z)
+*/
+package task5
