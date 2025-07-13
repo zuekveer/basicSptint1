0e680f9f0c45f8ff8685abcb7a6f80c862f0d8f9
zuekveer <simetramonk@gmail.com>
2025-07-13 23:37:12 +0300

    task4-5

--- task5/main.go
+++ task5/main.go
@@ -3,7 +3,22 @@
 
 Что выведет этот код и почему?
 
-	func main() {
+		func main() {
+		var x []int
+		x = append(x, 0)
+		x = append(x, 1)
+		x = append(x, 2)
+		y := append(x, 3)
+		z := append(x, 4)
+		fmt.Println(y)
+		fmt.Println(z)
+	}
+*/
+package task5
+
+import "fmt"
+
+func main() {
 	var x []int
 	x = append(x, 0)
 	x = append(x, 1)
@@ -12,5 +27,7 @@
 	z := append(x, 4)
 	fmt.Println(y)
 	fmt.Println(z)
-*/
-package task5
+}
+
+// [0 1 2 4]
+// [0 1 2 4]
